9552c24cb085ba752cca948c55014931fe96d008
Goliathuzzzz <mlaakkonen@live.com>
2024-10-23 12:01:52 +0300

    nodes excercises

---
+++ Week1/lab4/sys-calls.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const os = require('os');
+
+fs.readFile("sample.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else if (data) {
+        console.log(data);
+    }
+})
+
+fs.writeFile('output.txt', ("Hostname:" + os.hostname() + "\nPlatform: " + os.platform() + "\nCPUs: " + os.cpus()), (err) => {
+    if (err) {
+    console.error('Error writing file:', err);
+    } else {
+    console.log('Data written to output.txt');
+    }
+    });
+
+console.log("Hostname: ", + os.hostname());
+console.log("OS Type: ", + os.type());
+console.log("CPU cores: " + os.cpus());
